597e6aecafcdf387ea102e8a86b2b73c67acfe3a
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-28 14:14:27 -0700

    Database Synchronization and Server Listening

--- server.js
+++ server.js
@@ -40,3 +40,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
+
+// Database synchronization
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening on port', PORT));
+});
