30b1361aa96e3d3ea64d30c611c9128f6e092921
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-28 19:08:55 -0700

    Created a GET method for login route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 
+// GET method for all posts made
 router.get('/', async (req, res) => {
   res.render('homepage');
+});
+
+// GET method for login route
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
