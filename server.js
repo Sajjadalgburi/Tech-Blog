921f5429eba5ec798b54c0ccda59e6e8d00c5347
Sajjadalgburi <dfgsajjad@gmail.com>
2024-02-28 17:57:59 -0700

    Linked the routes to main server so express can use them

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
 const dotenv = require('dotenv');
+const routes = require('./controllers');
 
 // Load environment variables from .env file
 dotenv.config();
@@ -40,6 +41,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sess));
+app.use(routes);
 
 // Database synchronization
 sequelize.sync({ force: false }).then(() => {
