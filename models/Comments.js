a4143945a7741337a0ec46a4ddde4c8a5d6488be
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-01 17:51:29 -0700

    Finished setting up the comment model

--- models/Comments.js
+++ models/Comments.js
@@ -4,8 +4,40 @@ const { Model, DataTypes } = require('sequelize');
 // Importing database connection instance
 const sequelize = require('../config/connection');
 
-// Defining Post model class
+// Defining Comment model class
 class Comment extends Model {}
 
 // Initializing Comment model with properties and options
-Comment.init({}, {});
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: true,
+        notEmpty: true,
+      },
+    },
+    postId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Post',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  },
+);
