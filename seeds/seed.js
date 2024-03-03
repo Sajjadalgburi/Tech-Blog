47e394af3feb3337cafc4220cef2e67d4cad8a74
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-03 12:53:01 -0700

    Imported necessary modules and dependencies

---
+++ seeds/seed.js
@@ -0,0 +1,7 @@
+// Import necessary modules and dependencies
+const bcrypt = require('bcrypt'); // Module for hashing passwords
+const sequelize = require('../config/connection'); // Sequelize connection
+const { User, Post, Comment } = require('../models'); // Importing Sequelize models
+const userData = require('./userData.json'); // Sample user data
+const postData = require('./postData.json'); // Sample post data
+const commentData = require('./commentData.json'); // Sample comment data
