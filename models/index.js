ed88a130a15362a765f58a3808bd4986f574f70a
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-03 15:01:31 -0700

    Changed the naming of foreing-key's

--- models/index.js
+++ models/index.js
@@ -4,23 +4,23 @@ const Comment = require('./Comments');
 
 // ! Association #1
 User.hasMany(Post, {
-  foreignKey: 'userId',
+  foreignKey: 'user_id',
   onDelete: 'CASCADE',
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'userId',
+  foreignKey: 'user_id',
 });
 // ! Association #1
 
 // * Association #2
 Post.hasMany(Comment, {
-  foreignKey: 'postId',
+  foreignKey: 'post_id',
   onDelete: 'CASCADE',
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: 'postId',
+  foreignKey: 'post_id',
 });
 // * Association #2
 module.exports = { User, Post, Comment };
