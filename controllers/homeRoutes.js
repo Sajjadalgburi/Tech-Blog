f72cc46a0a25c55ce66e2740b2cd4308163b5fad
Sajjadalgburi <dfgsajjad@gmail.com>
2024-03-02 17:56:26 -0700

    Created GET method for logout route and also added reload method to refresh the page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,19 +3,21 @@ const withAuth = require('../utils/auth');
 
 // GET method for all posts made
 router.get('/', async (req, res) => {
-  res.render('homepage');
+  res.render('homepage', { logged_in: req.session.logged_in });
 });
 
 // GET method for user dashboard
 router.get('/dashboard', withAuth, async (req, res) => {
-  res.render('dashboard');
+  res.render('dashboard', {
+    logged_in: req.session.logged_in,
+  });
 });
 
 // GET method for login route
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/').reload();
     return;
   }
 
@@ -26,11 +28,16 @@ router.get('/login', (req, res) => {
 router.get('/signUp', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/').reload();
     return;
   }
 
   res.render('signUp');
+});
+
+// GET method for logout route
+router.get('/logout', (req, res) => {
+  res.redirect('/').reload();
 });
 
 module.exports = router;
